46c65cfbdc3f50da438f475137f1e8fa32fc5cab
Linus Stromberg <linstr07@edu.umea.se>
2023-04-27 15:02:32 +0200

    fixed

--- docs/_app/immutable/chunks/1.044f5e2c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3517b361.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.b9e34f46.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.2100da26.js";export{t as component};
